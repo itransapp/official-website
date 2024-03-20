95fce02ef7f382bd4c75f2f935926dac35a4d950
PotatoCloud <60210021+PotatoCloud@users.noreply.github.com>
2024-03-20 02:20:06 +0000

    Update main to output generated at 81ff916

--- static/js/Login-DfpIVf2g.js
+++
@@ -1,1 +0,0 @@
-import{l as r}from"./index-Bxvt32Fn.js";import{f as s}from"./data-SKDLe2qg.js";import{l as t}from"./chunk-MPX6TMFQ-Ckit8S-R.js";const n=()=>r.jsx("div",{className:"sticky top-[100vh] h-10 flex items-center justify-center space-x-6 font-medium",children:s.map((e,o)=>r.jsx(t,{isBlock:!0,size:"sm",href:e.href,color:"foreground",children:e.name},o))});export{n as default};
