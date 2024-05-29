49030a7c11ce9a14c97e7ca033ed93458bfb57bb
PotatoCloud <60210021+PotatoCloud@users.noreply.github.com>
2024-05-29 09:19:51 +0000

    Update main to output generated at 542b100

--- static/js/index-CRXHYiP7.js
+++
@@ -1,1 +0,0 @@
-import{J as a,r}from"./index-B33sEYiV.js";const o=({url:t})=>{const e=a();return r.useEffect(()=>{e(t)},[]),null};export{o as default};
---
+++ static/js/index-DRavrLU1.js
@@ -0,0 +1,1 @@
+import{K as a,r}from"./index-PT_xMk5q.js";const o=({url:t})=>{const e=a();return r.useEffect(()=>{e(t)},[]),null};export{o as default};
