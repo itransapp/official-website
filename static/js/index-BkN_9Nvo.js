38782fabf13f2534ac3e9ee171d018fb09419996
PotatoCloud <60210021+PotatoCloud@users.noreply.github.com>
2024-06-01 12:19:32 +0000

    Update main to output generated at 7502b41

--- static/js/index-BkN_9Nvo.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index-ooGS-eng.js";var a=globalThis!=null&&globalThis.document?o.useLayoutEffect:o.useEffect;export{a as u};
--- static/js/index-D9rUYDfi.js
+++
@@ -1,1 +0,0 @@
-import{K as a,r}from"./index-ooGS-eng.js";const o=({url:t})=>{const e=a();return r.useEffect(()=>{e(t)},[]),null};export{o as default};
