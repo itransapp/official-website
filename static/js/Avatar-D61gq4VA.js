71b8d34c1ce76154691be2aade0595bad451ef45
PotatoCloud <60210021+PotatoCloud@users.noreply.github.com>
2024-06-02 14:41:02 +0000

    Update main to output generated at dcbe589

--- static/js/Avatar-D61gq4VA.js
+++
@@ -1,1 +0,0 @@
-import{l as s,r as o,n as e}from"./index-D6APwUTW.js";import{A as m,D as r}from"./index-GDUb_ijs.js";import{a as c}from"./chunk-3YEK3JGC-BDcm4Vu4.js";const f=s((a,t)=>e.jsx(c,{ref:t,...a,src:m({src:a.src==="@"||a.src==="0"?r:a.src||r})})),i=o.memo(f);export{i as A};
---
+++ static/js/Avatar-D8bcB3jQ.js
@@ -0,0 +1,1 @@
+import{l as s,r as o,n as e}from"./index-wNxkLrUB.js";import{A as m,D as r}from"./index-BnYZiPK4.js";import{a as c}from"./chunk-3YEK3JGC-wEjpkDbB.js";const f=s((a,t)=>e.jsx(c,{ref:t,...a,src:m({src:a.src==="@"||a.src==="0"?r:a.src||r})})),i=o.memo(f);export{i as A};
